d5f289af06d837db88260caf87ebc6efcbc5b873
Martin Suesskraut <martin.suesskraut@gmail.com>
2018-03-10 00:16:12 +0100

    added tsconfig

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { enableLiveReload } from 'electron-compile';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow : BrowserWindow;
 
 const isDevMode = process.execPath.match(/[\\/]electron/);
 
