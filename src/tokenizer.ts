f15bfaf0a161ed7162f321d5c0b8f4846cd445d7
Martin Suesskraut <martin.suesskraut@gmail.com>
2018-03-13 22:31:53 +0100

    finished tests for tokenizer

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,85 +1,90 @@
 export enum TokenKind {
-    Number,
-    Operator
+  Number,
+  Operator
 }
 
 export class TokenLocation {
-    constructor(public readonly offset : number) { }
+  constructor(public readonly offset: number) {}
 
-    toString(): string {
-        return `offset: ${this.offset}`
-    }
+  toString(): string {
+    return `offset: ${this.offset}`;
+  }
 }
 
 export class TokenError {
-    constructor(
-        public readonly message: string,
-        public readonly location : TokenLocation)
-    { }
+  constructor(
+    public readonly message: string,
+    public readonly location: TokenLocation
+  ) {}
 
-    GetFullErrorMessage() : string {
-        return `Error at ${this.location.offset}: ${this.message}`;
-    }
+  GetFullErrorMessage(): string {
+    return `Error at ${this.location.offset}: ${this.message}`;
+  }
 }
 
 export class Token {
-    constructor(
-        public readonly kind : TokenKind,
-        public readonly text : string,
-        public readonly value : any,
-        public readonly location : TokenLocation)
-    { }
+  constructor(
+    public readonly kind: TokenKind,
+    public readonly text: string,
+    public readonly value: any,
+    public readonly location: TokenLocation
+  ) {}
 
-    toString() : string {
-        return `${TokenKind[this.kind]}; text: ${this.text}; value: ${this.value} at ${this.location}`;
-    }
+  toString(): string {
+    return `${TokenKind[this.kind]}; text: ${this.text}; value: ${
+      this.value
+    } at ${this.location}`;
+  }
 }
 
-export class Tokennizer {
-    private text : string;
-    private loc : number;
+export class Tokenizer {
+  private text: string;
+  private loc: number;
 
-    constructor(txt : string) {
-        this.text = txt;
-        this.loc = 0;
-    }
+  constructor(txt: string) {
+    this.text = txt;
+    this.loc = 0;
+  }
 
-    private AdvanceChar() : void {
-        this.loc += 1;
-    }
+  private AdvanceChar(): void {
+    this.loc += 1;
+  }
 
-    private IsOperator(c : string) : boolean {
-        return c in ['+', '-', '*', '/'];
-    }
+  private IsOperator(c: string): boolean {
+    return ["+", "-", "*", "/"].indexOf(c) >= 0;
+  }
 
-    private IsDigit(c : string) : boolean {
-        return c >= '0' && c <= '9';
-    }
+  private IsDigit(c: string): boolean {
+    return c >= "0" && c <= "9";
+  }
 
-    IsFinished() : boolean {
-        return this.loc === this.text.length;
-    }
+  IsFinished(): boolean {
+    return this.loc === this.text.length;
+  }
 
-    NextToken() : Token {
-        let c = this.text[this.loc];
-        if (this.IsOperator(c)) {
-            let t = new Token(TokenKind.Operator, c, c, new TokenLocation(this.loc));
-            this.AdvanceChar();
-            return t;
-        }
-        else if (this.IsDigit(c)) {
-            let l = this.loc;
-            let strNum = '';
-            while (this.IsDigit(c)) {
-                strNum += c;
-                this.AdvanceChar();
-                c = this.text[this.loc];
-            }
-            let t = new Token(TokenKind.Number, strNum, parseInt(strNum), new TokenLocation(l));
-            return t;
-        }
-        else {
-            throw new TokenError(`Unknown char ${c}`, new TokenLocation(this.loc));
-        }
+  NextToken(): Token {
+    let c = this.text[this.loc];
+    if (this.IsOperator(c)) {
+      let t = new Token(TokenKind.Operator, c, c, new TokenLocation(this.loc));
+      this.AdvanceChar();
+      return t;
+    } else if (this.IsDigit(c)) {
+      let l = this.loc;
+      let strNum = "";
+      while (this.IsDigit(c)) {
+        strNum += c;
+        this.AdvanceChar();
+        c = this.text[this.loc];
+      }
+      let t = new Token(
+        TokenKind.Number,
+        strNum,
+        parseInt(strNum),
+        new TokenLocation(l)
+      );
+      return t;
+    } else {
+      throw new TokenError(`Unknown char ${c}`, new TokenLocation(this.loc));
     }
+  }
 }
