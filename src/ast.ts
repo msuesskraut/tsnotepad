029aef93b5c0915b4e42dd95c2e1e5d9072eed3e
Martin Suesskraut <martin.suesskraut@gmail.com>
2018-03-19 07:39:06 +0100

    removed unnecessary ast suffix

--- src/ast.ts
+++ src/ast.ts
@@ -1,54 +1,54 @@
 import { Token } from "./tokenizer";
 
-export abstract class ASTVisitor<T> {
+export abstract class Visitor<T> {
   abstract visitNumber(n: Number): T;
   abstract visitUnOp(op: UnOp): T;
   abstract visitBinOp(op: BinOp): T;
 }
 
-export abstract class ASTNode {
+export abstract class Node {
   constructor(public readonly token: Token) {}
 
-  abstract accept<T>(v: ASTVisitor<T>): T;
+  abstract accept<T>(v: Visitor<T>): T;
 }
 
 /// a terminal
-export class Number extends ASTNode {
+export class Number extends Node {
   readonly value: number;
   constructor(token: Token) {
     super(token);
     this.value = token.value as number;
   }
 
-  accept<T>(v: ASTVisitor<T>): T {
+  accept<T>(v: Visitor<T>): T {
     return v.visitNumber(this);
   }
 }
 
-export class UnOp extends ASTNode {
+export class UnOp extends Node {
   readonly op: string;
-  constructor(token: Token, public readonly expr: ASTNode) {
+  constructor(token: Token, public readonly expr: Node) {
     super(token);
     this.op = token.value as string;
   }
 
-  accept<T>(v: ASTVisitor<T>): T {
+  accept<T>(v: Visitor<T>): T {
     return v.visitUnOp(this);
   }
 }
 
-export class BinOp extends ASTNode {
+export class BinOp extends Node {
   readonly op: string;
   constructor(
     token: Token,
-    public readonly left: ASTNode,
-    public readonly right: ASTNode
+    public readonly left: Node,
+    public readonly right: Node
   ) {
     super(token);
     this.op = token.value as string;
   }
 
-  accept<T>(v: ASTVisitor<T>): T {
+  accept<T>(v: Visitor<T>): T {
     return v.visitBinOp(this);
   }
 }
